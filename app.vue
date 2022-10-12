0e1da0589895254c78e5c8eee3ab936008f99391
Ilya Konovalov <i.konovalov@admitad.com>
2022-10-12 13:41:26 +0300

    git init nuxt project

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+    <button @click="fetchTodoList">Fetch</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineNuxtComponent } from "#app";
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch("https://jsonplaceholder.typicode.com/todos/")
+        .then((response) => response.json())
+        .then((json) => {
+          this.todoList = json;
+        });
+    },
+  },
+});
+</script>
